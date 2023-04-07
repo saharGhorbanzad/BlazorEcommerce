a86f5e786f3b8583768d0f745d38b3ab92568ffe
saharGhorbanzad <93582923+saharGhorbanzad@users.noreply.github.com>
2023-04-07 20:36:35 +0330

    Add project files.

---
+++ BlazorEcommerce/Shared/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorEcommerce.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string ImageUrl { get; set; } = String.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
